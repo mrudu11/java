2faf077b724943cfb95bfc3284415e9a67efc2a7
Mrudula <mrudulawaghmode11@gmail.com>
2025-07-04 19:45:08 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+package com.dkte;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1, b= 2, c=5;
+		if(a+b==c)
+		{
+			System.out.println("fibonecci");
+		}
+		else {
+			System.out.println("not a fibonecci series");
+		}
+
+	}
+
+}
