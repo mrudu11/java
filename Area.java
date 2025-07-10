46283ee1068fdb78c138df38590a1959b9164381
Mrudula <mrudulawaghmode11@gmail.com>
2025-07-10 19:33:00 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+package com.dkte;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double r =5,a;
+		a = r*r*3.14;
+		System.out.println(a);
+
+	}
+
+}
