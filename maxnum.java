078bfeb29af5b1c7c8a68d81c53d0cc4b52960c0
Mrudula <mrudulawaghmode11@gmail.com>
2025-07-11 21:15:51 +0530

    Add files via upload

---
+++ maxnum.java
@@ -0,0 +1,23 @@
+package com.dkte;
+
+public class maxnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5,b=6,c=8;
+		if(a>b && a>c)
+		{
+			System.out.println("a is max number");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is max number");
+		}
+		else
+		{
+			System.out.println("c is max number");
+		}
+
+	}
+
+}
